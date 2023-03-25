fd797c5973a836903df960ca54e3504b309ec031
erssrm <er.ssrm@gmail.com>
2023-03-25 21:17:51 +0530

    cypress setup with ci start sarver and test

---
+++ apps/web/cypress/e2e/landing-page.cy.ts
@@ -0,0 +1,7 @@
+describe('Landing Page Spec', () => {
+  it('Landing Page should render', () => {
+    cy.visit('http://localhost:3000/')
+  })
+})
+
+export {}
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,13 +1,11 @@
 import { createTheme, ThemeProvider } from "@mui/material";
-import { Button } from "ui";
-import { NewsCard } from "ui";
 import { AppContainer } from "../components/Container";
 
 
 const themeLight = createTheme({
   palette: {
     background: {
-      default: "#81c784"
+      default: "#C0C0C0"
     }
   }
 });
@@ -17,6 +15,5 @@ export default function Web() {
     <ThemeProvider theme={themeLight}>
       <AppContainer />
     </ThemeProvider>
-
   );
 }
