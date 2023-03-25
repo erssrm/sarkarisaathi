85526647f912296eca5700d1d9d9e3d367db7400
erssrm <er.ssrm@gmail.com>
2023-03-25 19:24:06 +0530

    primary layout completed

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,22 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { Button } from "ui";
+import { NewsCard } from "ui";
+import { AppContainer } from "../components/Container";
+
+
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: "#81c784"
+    }
+  }
+});
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <ThemeProvider theme={themeLight}>
+      <AppContainer />
+    </ThemeProvider>
+
   );
 }
