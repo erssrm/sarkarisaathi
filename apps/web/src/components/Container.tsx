c4258873b021b779e064024562ddde3cda1e6608
erssrm <er.ssrm@gmail.com>
2023-04-22 00:31:33 +0530

    eslint configuration with airbnb

--- apps/web/src/components/Container.tsx
+++ apps/web/src/components/Container.tsx
@@ -1,31 +1,30 @@
 import React from "react";
-import Container from "@mui/material/Container"
+import Container from "@mui/material/Container";
 import { NewsCard } from "ui";
 import { CssBaseline, Grid } from "@mui/material";
 
 export const AppContainer = () => {
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="md">
-                <h1>Sarkari Saathi</h1>
-                <h5>Get all information about the govermnet schemes</h5>
-                <Grid container spacing={3}>
-                    <Grid item>
-                        <NewsCard />
-                    </Grid>
-                    <Grid item>
-                        <NewsCard />
-                    </Grid>
-                    <Grid item>
-                        <NewsCard />
-                    </Grid>
-                    <Grid item>
-                        <NewsCard />
-                    </Grid>
-                </Grid>
-
-            </Container>
-        </React.Fragment>
-    );
-}
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <h1>Sarkari Saathi</h1>
+        <h5>Get all information about the govermnet schemes</h5>
+        <Grid container spacing={3}>
+          <Grid item>
+            <NewsCard />
+          </Grid>
+          <Grid item>
+            <NewsCard />
+          </Grid>
+          <Grid item>
+            <NewsCard />
+          </Grid>
+          <Grid item>
+            <NewsCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+};
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "airbnb"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "linebreak-style": "off",
   },
   parserOptions: {
     babelOptions: {
